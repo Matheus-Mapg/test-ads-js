1ab111b5dd6260ba174a85bf85fda81bac749b70
Matheus-Mapg <mapg.goes@gmail.com>
2023-06-29 18:57:10 -0300

    🩹 mudando forma de leitura para capturar o conteudo do arquivo

--- src/question-two/use-cases/decipher-commands/decipher-commands.usecase.ts
+++ src/question-two/use-cases/decipher-commands/decipher-commands.usecase.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { readFile } from 'fs/promises';
+import { readFile, unlink, writeFile } from 'fs/promises';
+import { cwd } from 'process';
 import { MulterFile } from 'src/question-two/data/types/multer-file/multer-file';
 
 @Injectable()
 export class DecipherCommandsUseCase {
+
+    private async readFileMulter(file: MulterFile) {
+
+        const filePath = `${cwd()}/${file.originalname}`
+
+        await writeFile(filePath, file.buffer);
+
+        const text = await readFile(filePath, 'utf-8');
+
+        await unlink(filePath);
+
+        return text
+    }
+
     private async splitCommands(text: string) {
         const returnLines = text.split('\n')
 
@@ -22,7 +37,7 @@ export class DecipherCommandsUseCase {
     }
     async extractCommands(file: MulterFile) {
 
-        const text = await readFile(file.buffer, { encoding: 'utf-8' })
+        const text = await this.readFileMulter(file)
 
         const commands = await this.splitCommands(text)
 
